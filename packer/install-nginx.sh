40be33de901f9cd4391d4e91dbbcd684cf865ea1
Itamar Syn-Hershko <itamar@code972.com>
2016-12-26 23:03:20 +0200

    Enabling password protection on client node

--- packer/install-nginx.sh
+++ packer/install-nginx.sh
@@ -2,7 +2,7 @@
 set -e
 
 sudo apt-get install nginx apache2-utils
-sudo htpasswd -c /etc/nginx/conf.d/search.htpasswd changeme
+sudo htpasswd -bc /etc/nginx/conf.d/search.htpasswd exampleuser changeme
 
 sudo mv ~/nginx-client.conf /etc/nginx/nginx.conf
 
