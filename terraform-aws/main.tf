cf2ae4959ca17b491681b5fe86ca0af6d16aea9f
Itamar Syn-Hershko <itamar@code972.com>
2017-02-02 16:00:27 +0200

    ES nodes need to be able to ping each other

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -46,6 +46,15 @@ resource "aws_security_group_rule" "ssh_access" {
   security_group_id = "${aws_security_group.elasticsearch_security_group.id}"
 }
 
+resource "aws_security_group_rule" "ping_within_vpc" {
+  type              = "ingress"
+  from_port         = 8
+  to_port           = 0
+  protocol          = "icmp"
+  self              = true
+  security_group_id = "${aws_security_group.elasticsearch_security_group.id}"
+}
+
 resource "aws_security_group_rule" "es_egress" {
   type              = "egress"
   from_port         = 0
