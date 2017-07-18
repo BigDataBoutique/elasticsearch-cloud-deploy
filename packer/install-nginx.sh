70777042c469ac06aca0e16509d5e83fd793a898
Itamar Syn-Hershko <itamar@code972.com>
2017-07-19 00:54:04 +0300

    Cleaning up packer scripts

--- packer/install-nginx.sh
+++ packer/install-nginx.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-sudo apt-get install nginx apache2-utils
-sudo htpasswd -bc /etc/nginx/conf.d/search.htpasswd exampleuser changeme
+apt-get install nginx apache2-utils
+htpasswd -bc /etc/nginx/conf.d/search.htpasswd exampleuser changeme
 
-sudo mv ~/nginx-client.conf /etc/nginx/nginx.conf
+mv ~/nginx-client.conf /etc/nginx/nginx.conf
 
-sudo service nginx start
+service nginx start
