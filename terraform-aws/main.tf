6062aadd3f35b129defc303ac8d840826fdb322d
Itamar Syn-Hershko <itamar@code972.com>
2017-07-24 22:42:34 +0300

    Fixing ELB health check

--- terraform-aws/main.tf
+++ terraform-aws/main.tf
@@ -114,7 +114,7 @@ resource "aws_elb" "es_client_lb" {
     healthy_threshold   = 2
     unhealthy_threshold = 2
     timeout             = 3
-    target              = "HTTP:9200/"
+    target              = "HTTP:8080/status"
     interval            = 6
   }
 
