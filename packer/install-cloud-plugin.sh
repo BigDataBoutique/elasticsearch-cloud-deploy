8407758279e2ad9c699c2cb39cc5a99298ebdf75
Itamar Syn-Hershko <itamar@code972.com>
2018-03-09 11:37:24 +0200

    Fixes issues with install-cloud-plugins
    Closes #28

--- packer/install-cloud-plugin.sh
+++ packer/install-cloud-plugin.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
+set -e
 
 cd /usr/share/elasticsearch/
 
-# install AWS-specific plugins only if running on AWS
-# see http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
-if [ -f /sys/hypervisor/uuid ] && [ `head -c 3 /sys/hypervisor/uuid` == ec2 ]; then
-  bin/elasticsearch-plugin install --batch discovery-ec2
-  bin/elasticsearch-plugin install --batch repository-s3
-fi
-
-# install Azure-specific plugins only if running on Azure
-if `grep -q unknown-245 /var/lib/dhcp/dhclient.eth0.leases`; then
-  bin/elasticsearch-plugin install --batch repository-azure
-  bin/elasticsearch-plugin install --batch discovery-azure-classic
-fi
-
-# install Google Compute specific plugins only if running on GCP
-if (dmidecode -s system-product-name | grep -q "Google Compute Engine"); then
-  bin/elasticsearch-plugin install --batch discovery-gce
-  bin/elasticsearch-plugin install --batch repository-gcs
+if [[ -f /sys/hypervisor/uuid && `head -c 3 /sys/hypervisor/uuid` == "ec2" ]]; then
+  # install AWS-specific plugins only if running on AWS
+  # see http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/identify_ec2_instances.html
+  sudo bin/elasticsearch-plugin install --batch discovery-ec2
+  sudo bin/elasticsearch-plugin install --batch repository-s3
+elif `grep -q unknown-245 /var/lib/dhcp/dhclient.eth0.leases`; then
+  # install Azure-specific plugins only if running on Azure
+  sudo bin/elasticsearch-plugin install --batch repository-azure
+  sudo bin/elasticsearch-plugin install --batch discovery-azure-classic
+elif (sudo dmidecode -s system-product-name | grep -q "Google Compute Engine"); then
+  # install Google Compute specific plugins only if running on GCP
+  sudo bin/elasticsearch-plugin install --batch discovery-gce
+  sudo bin/elasticsearch-plugin install --batch repository-gcs
 fi
