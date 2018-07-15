00055c1f0ce39179ac5d974df5713340deb98c14
Félix Barbeira <fbarbeira@gmail.com>
2018-07-15 21:16:30 +0200

    Use 'xenial' webupd8team repository.
    Ubuntu 16 machines are used. 'Trusty' is ubuntu 14 version.

--- packer/install-java8.sh
+++ packer/install-java8.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Add the Java 8 repository.
-sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
-sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
+sudo echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/webupd8team-java.list
+sudo echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main" | sudo tee -a /etc/apt/sources.list.d/webupd8team-java.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
 sudo apt-get update
 
