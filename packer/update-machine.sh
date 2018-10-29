35afc75aeaf8f1f761852305513a7ae95dcdebf8
Itamar Syn-Hershko <itamar@code972.com>
2018-10-29 14:43:12 +0200

    Some Ubuntu images now have python2 instead of python

--- packer/update-machine.sh
+++ packer/update-machine.sh
@@ -10,5 +10,5 @@ sudo apt-get install -y software-properties-common git python-dev htop ntp jq ap
 echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
 
 wget https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-pip install boto awscli
+python get-pip.py || python2 get-pip.py
+pip install boto awscli || pip2 install boto awscli 
