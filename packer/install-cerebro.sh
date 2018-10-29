9616d7cca3842ed82d8cdb46bc6f2952cb3ed09c
Itamar Syn-Hershko <itamar@code972.com>
2018-10-29 14:44:58 +0200

    Updating cerebro version

--- packer/install-cerebro.sh
+++ packer/install-cerebro.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-export CEREBRO_VERSION="0.7.2"
+export CEREBRO_VERSION="0.8.1"
 
 wget https://github.com/lmenezes/cerebro/releases/download/v${CEREBRO_VERSION}/cerebro-${CEREBRO_VERSION}.tgz
 mkdir /usr/share/cerebro
