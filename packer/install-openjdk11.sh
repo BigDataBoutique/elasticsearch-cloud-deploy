b2bdb689a8fc05eef14e06254c5787a689cd7991
Tigran <tigran@bigdataboutique.com>
2019-07-22 00:07:36 +0400

    Update JAVA_HOME to OpenJDK 11

--- packer/install-openjdk11.sh
+++ packer/install-openjdk11.sh
@@ -4,3 +4,7 @@ set -e
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt install -y openjdk-11-jdk
+
+export JAVA_HOME=$(jrunscript -e 'java.lang.System.out.println(java.lang.System.getProperty("java.home"));')
+/usr/bin/printf "
+JAVA_HOME=${JAVA_HOME}" >> /etc/environment
