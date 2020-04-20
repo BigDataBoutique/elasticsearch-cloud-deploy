9706e2781bfaaccf977119687a4086985a2cc31d
Tigran Simonyan <tigran@bigdataboutique.com>
2020-04-20 11:46:41 +0400

    Packer beats version based on es_version config

--- packer/install-beats.sh
+++ packer/install-beats.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 set -e
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-7.5.0-amd64.deb
-sudo dpkg -i filebeat-7.5.0-amd64.deb
-rm filebeat-7.5.0-amd64.deb
+ES_VERSION="${ES_VERSION:-7.6.2}"
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-7.5.0-amd64.deb
-sudo dpkg -i heartbeat-7.5.0-amd64.deb
-rm heartbeat-7.5.0-amd64.deb
+curl -L -O "https://artifacts.elastic.co/downloads/beats/filebeat/filebeat-$ES_VERSION-amd64.deb"
+sudo dpkg -i "filebeat-$ES_VERSION-amd64.deb"
+rm "filebeat-$ES_VERSION-amd64.deb"
 
-curl -L -O https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-7.5.0-amd64.deb
-sudo dpkg -i metricbeat-7.5.0-amd64.deb
-rm metricbeat-7.5.0-amd64.deb
+curl -L -O "https://artifacts.elastic.co/downloads/beats/heartbeat/heartbeat-$ES_VERSION-amd64.deb"
+sudo dpkg -i "heartbeat-$ES_VERSION-amd64.deb"
+rm "heartbeat-$ES_VERSION-amd64.deb"
+
+curl -L -O "https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-$ES_VERSION-amd64.deb"
+sudo dpkg -i "metricbeat-$ES_VERSION-amd64.deb"
+rm "metricbeat-$ES_VERSION-amd64.deb"
