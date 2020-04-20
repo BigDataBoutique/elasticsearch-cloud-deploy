ae3cbd21c5ccd552c3aea1590c949da2e5f7bb22
Tigran Simonyan <tigran@bigdataboutique.com>
2020-04-20 11:48:29 +0400

    Install awscli from apt-get

--- packer/update-machine.sh
+++ packer/update-machine.sh
@@ -10,12 +10,7 @@ sleep 100
 
 sudo apt-get update
 sudo -E apt-get upgrade -y
-sudo -E apt-get install -y software-properties-common git python-dev htop ntp jq apt-transport-https unzip
+sudo -E apt-get install -y software-properties-common git python-dev htop ntp jq apt-transport-https unzip awscli
 
 # Disable daily apt unattended updates.
 echo 'APT::Periodic::Enable "0";' >> /etc/apt/apt.conf.d/10periodic
-
-cd /tmp
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "/tmp/awscliv2.zip"
-unzip /tmp/awscliv2.zip -d /tmp
-sudo /tmp/aws/install
