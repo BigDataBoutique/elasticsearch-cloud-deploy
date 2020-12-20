a7140e73d46c2463ed028853bc4accb29ec0a6f9
Itamar Syn-Hershko <itamar@bigdataboutique.com>
2020-12-20 17:04:35 +0200

    Update dev.tf

--- terraform-aws/dev.tf
+++ terraform-aws/dev.tf
@@ -1,22 +1,22 @@
-data "template_file" "dev-s3" {
-  template = file("${path.module}/../assets/s3-backup.json")
+# data "template_file" "dev-s3" {
+#   template = file("${path.module}/../assets/s3-backup.json")
 
-  vars = {
-    s3_backup_bucket = var.DEV_MODE_scripts_s3_bucket
-  }
-}
+#   vars = {
+#     s3_backup_bucket = var.DEV_MODE_scripts_s3_bucket
+#   }
+# }
 
-resource "aws_s3_bucket" "dev" {
-  count = var.DEV_MODE_scripts_s3_bucket == "" ? 0 : 1
+# resource "aws_s3_bucket" "dev" {
+#   count = var.DEV_MODE_scripts_s3_bucket == "" ? 0 : 1
 
-  bucket = "${var.DEV_MODE_scripts_s3_bucket}"
-  region = var.aws_region
-  acl    = "private"
-}
+#   bucket = "${var.DEV_MODE_scripts_s3_bucket}"
+#   region = var.aws_region
+#   acl    = "private"
+# }
 
-resource "aws_iam_role_policy" "dev-s3" {
-  count  = var.DEV_MODE_scripts_s3_bucket != "" ? 1 : 0
-  name   = "${var.es_cluster}-elasticsearch-s3-devmode-policy"
-  role   = aws_iam_role.elasticsearch.id
-  policy = data.template_file.dev-s3.rendered
-}
+# resource "aws_iam_role_policy" "dev-s3" {
+#   count  = var.DEV_MODE_scripts_s3_bucket != "" ? 1 : 0
+#   name   = "${var.es_cluster}-elasticsearch-s3-devmode-policy"
+#   role   = aws_iam_role.elasticsearch.id
+#   policy = data.template_file.dev-s3.rendered
+# }
