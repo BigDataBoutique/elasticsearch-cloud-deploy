fd09bc135e3f3b683a0402c039cfa88a6f4bf803
ronenbdbq <123952033+ronenbdbq@users.noreply.github.com>
2023-02-02 22:03:18 +1100

    adding sumo environment variable

--- packer/install-sumo.sh
+++ packer/install-sumo.sh
@@ -3,4 +3,4 @@ set -e
 
 curl -o /tmp/SumoCollector.sh https://collectors.us2.sumologic.com/rest/download/linux/64
 chmod +x /tmp/SumoCollector.sh
-sudo /tmp/SumoCollector.sh -q -Vsumo.token_and_url=foobar
+sudo /tmp/SumoCollector.sh -q -Vsumo.token_and_url=$SUMO_TOKEN
