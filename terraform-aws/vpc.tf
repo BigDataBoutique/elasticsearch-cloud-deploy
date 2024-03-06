17613a46b9dc001ff5e4a28aaf6168cbb2419706
shai <shai@bigdataboutique.com>
2024-03-06 10:09:57 +0200

    various fixes

--- terraform-aws/vpc.tf
+++ terraform-aws/vpc.tf
@@ -31,9 +31,16 @@ resource "aws_security_group" "vpc-endpoint" {
   vpc_id = var.vpc_id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 9200
+    to_port     = 9200
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5601
+    to_port     = 5601
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
